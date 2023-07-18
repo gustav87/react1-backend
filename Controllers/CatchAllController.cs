123c949207e9a42e91e3bfd1220ac74f05573641
Gustav Andersson <gustav87and@gmail.com>
2023-07-18 11:19:43 +0200

    add catch-all route

---
+++ Controllers/CatchAllController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace react1_backend.Controllers;
+
+[ApiController]
+[Route("{**catchAll}")]
+public class CatchAllController : ControllerBase
+{
+    private readonly ILogger<CatchAllController> _logger;
+
+    public CatchAllController(ILogger<CatchAllController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult CatchAll(string catchAll)
+    {
+        HttpError err = new HttpError();
+        err.msg = $"Route '{catchAll}' does not exist";
+        err.errorCode = HttpStatusCode.NotFound;
+        string json = JsonConvert.SerializeObject(err);
+
+        return NotFound(json);
+        // return Content(json, "application/json");
+    }
+
+    public class HttpError
+    {
+        public string? msg {get; set;}
+        public HttpStatusCode? errorCode {get; set;}
+    }
+}
