30ee5c9fe45bf184434626f9b652cdd0e75accdb
Gustav Andersson <gustav87and@gmail.com>
2024-11-10 10:52:11 +0100

    formatting

--- Filters/ActionFilters/ActionFilterExample.cs
+++ Filters/ActionFilters/ActionFilterExample.cs
@@ -4,41 +4,42 @@ namespace React1_backend.Filters.ActionFilters;
 
 public class ActionFilterExample(IHttpContextAccessor httpContextAccessor) : IActionFilter
 {
-    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
-    private readonly string adminToken = Environment.GetEnvironmentVariable("adminToken") ?? "";
+  private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+  private readonly string adminToken = Environment.GetEnvironmentVariable("adminToken") ?? "";
 
-    public void OnActionExecuting(ActionExecutingContext context)
+  public void OnActionExecuting(ActionExecutingContext context)
+  {
+    // Do something before the action executes.
+    string? queryString = context.HttpContext.Request.QueryString.Value;
+    bool containsToken = context.HttpContext.Request.Query.ContainsKey("token");
+
+    // var token = _httpContextAccessor.HttpContext.Request.Headers["token"].ToString();
+    // if (string.IsNullOrEmpty(token))
+    if (!containsToken)
     {
-        // Do something before the action executes.
-        var queryString = context.HttpContext.Request.QueryString.Value;
-        var containsToken = context.HttpContext.Request.Query.ContainsKey("token");
-
-        // var token = _httpContextAccessor.HttpContext.Request.Headers["token"].ToString();
-        // if (string.IsNullOrEmpty(token))
-        if (!containsToken)
-        {
-            context.HttpContext.Response.StatusCode = 401; // BadRequest
-            context.HttpContext.Response.WriteAsync("Token missing.");
-            return;
-        }
-
-        var tokenValue = context.HttpContext.Request.Query["token"][0];
-
-        if(!ValidateToken(tokenValue))
-        {
-            context.HttpContext.Response.StatusCode = 401; // BadRequest
-            context.HttpContext.Response.WriteAsync("Token validation failed.");
-            return;
-        }
+      context.HttpContext.Response.StatusCode = 401; // BadRequest
+      context.HttpContext.Response.WriteAsync("Token missing.");
+      return;
     }
 
-    public void OnActionExecuted(ActionExecutedContext context)
+    string? tokenValue = context.HttpContext.Request.Query["token"][0];
+
+    if(!ValidateToken(tokenValue))
     {
-        // Do something after the action executes.
-        Console.WriteLine("ActionFilterExample executed");
+      context.HttpContext.Response.StatusCode = 401; // BadRequest
+      context.HttpContext.Response.WriteAsync("Token validation failed.");
+      return;
     }
+  }
 
-    private bool ValidateToken(string token) {
-      return token == adminToken;
-    }
+  public void OnActionExecuted(ActionExecutedContext context)
+  {
+    // Do something after the action executes.
+    Console.WriteLine("ActionFilterExample executed");
+  }
+
+  private bool ValidateToken(string? token) {
+    if (token is null) return false;
+    return token == adminToken;
+  }
 }
