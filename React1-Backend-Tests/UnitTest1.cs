8b9e65ccbcd79ad46d3adb770e88634409065ad7
Gustav Andersson <gustav87and@gmail.com>
2024-12-01 18:09:58 +0100

    add not null test

--- React1-Backend-Tests/UnitTest1.cs
+++ React1-Backend-Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using React1_Backend.Contracts;
 namespace React1_Backend_Tests;
 
 public class Tests
@@ -8,20 +9,28 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TestPass()
     {
         Assert.Pass();
     }
 
     [Test]
-    public void TestStartsWithUpper()
+    public void TestStartsWithA()
     {
-        // Tests that we expect to return true.
         string[] words = ["Alphabet", "Aebra", "ABC"];
         foreach (string word in words)
         {
             bool result = word.StartsWith('A');
             Assert.IsTrue(result, string.Format("Expected for '{0}': true; Actual: {1}", word, result));
         }
+    }
+
+    [Test]
+    public void TestCloudFile()
+    {
+        CloudFile cloudFile = new();
+        // Tests that we expect to return true.
+        Assert.IsNotNull(cloudFile);
+        Assert.That(cloudFile, Is.Not.Null);
     }
 }
