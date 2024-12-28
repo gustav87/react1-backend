ccb248ee12f28c369fd1af5d8d28e6ee06c370d9
Gustav Andersson <gustav87and@gmail.com>
2024-12-28 01:09:22 +0100

    try use external mongodb for integration tests, since the build pipeline is not run on portainer, so it cannot access react1-db at all

--- React1-Backend-IntegrationTests/IntegrationTestBase.cs
+++ React1-Backend-IntegrationTests/IntegrationTestBase.cs
@@ -6,14 +6,14 @@ namespace React1_Backend_IntegrationTests;
 
 public class IntegrationTestBase
 {
-    private readonly string MONGO_CONNECTION_STRING = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING") ?? "mongodb://localhost:27017";
+    private readonly string MONGO_CONNECTION_STRING_INTTEST = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING_INTTEST") ?? "mongodb://localhost:27017";
     private readonly string databaseName = "IntegrationTests";
     protected MongoClient _mongoClient;
 
     [SetUp]
     public async Task Setup()
     {
-        MongoClientSettings mongoClientSettings = MongoClientSettings.FromConnectionString($"mongodb://{MONGO_CONNECTION_STRING}:27017");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.FromConnectionString(MONGO_CONNECTION_STRING_INTTEST);
         mongoClientSettings.ServerSelectionTimeout = TimeSpan.FromSeconds(5);
         mongoClientSettings.ConnectTimeout = TimeSpan.FromSeconds(5);
         _mongoClient = new MongoClient(mongoClientSettings);
